6016f11759fe6f51c3175776a3507490c1cfeb50
Karthik Sapaliga <im.karthiksapaliga@gmail.com>
2024-01-23 18:25:53 +0530

    Modified Stack.c file

--- DataStructures/Stack.c
+++ DataStructures/Stack.c
@@ -13,15 +13,17 @@ void init(Stack *s){
 }
 
 void menu(){
-    printf("===================\n");
+    printf("=============================================\n");
     printf("Stack Operations : \n");
-    printf("-------------------\n");
+    printf("---------------------------------------------\n");
     printf("1. push()\n");
     printf("2. pop()\n");
     printf("3. peek()\n");
-    printf("4. display()\n");
-    printf("\n[0] MENU [-1] EXIT\n");
-    printf("===================\n");
+    printf("4. isEmpty()\n");
+    printf("5. isFull()\n");
+    printf("6. display()\n");
+    printf("\n[0] MENU\t[-1] EXIT\n");
+    printf("=============================================\n");
 }
 
 int isFull(Stack s){
@@ -34,34 +36,34 @@ int isEmpty(Stack s){
 
 void push(Stack *s, int ele){
     if(isFull(*s)){
-        fprintf(stderr,"ERROR : Stack Overflow\n");
-        exit(EXIT_FAILURE);
+        printf("Error : <stack_overflow> cannot push element.\n");
     }else{
         s->arr[++s->top] = ele;
+        printf("%d is pushed onto the stack.\n",ele);
     }
 }
 
 int pop(Stack *s){
     if(isEmpty(*s)){
-        fprintf(stderr,"ERROR : Stack Underflow\n");
-        exit(EXIT_FAILURE);
+        printf("Error : <stack_underflow> cannot pop element.\n");
     }else{
+        printf("%d is popped from the stack.\n",s->arr[s->top]);
         return s->arr[s->top--];
     }
 }
 
 int peek(Stack s){
     if(isEmpty(s)){
-        fprintf(stderr,"ERROR : Stack Underflow\n");
-        exit(EXIT_FAILURE);
+        printf("Error : <empty_stack>\n");
     }else{
+        printf("%d is peeked from the stack.\n",s.arr[s.top]);
         return s.arr[s.top];
     }
 }
 
 void display(Stack s){
     if(isEmpty(s)){
-        fprintf(stderr,"ERROR : Stack Underflow");
+        printf("Error : <empty_stack>\n");
     }else{
         for(int i=0; i<=s.top; i++){
             printf("%d ",s.arr[i]);
@@ -77,27 +79,36 @@ int main(){
     init(&stack);
     menu();
     do{
-        printf("Enter the choice : ");
+        printf("Please enter your choice: ");
         scanf("%d",&choice);
+
         switch(choice){
             case 0: menu();
                     break;
-            case 1: printf("Enter the element : ");
+            case 1: printf("Enter the element to push onto the stack : ");
                     scanf("%d",&element);
                     push(&stack, element);
                     break;
-            case 2: element = pop(&stack);
-                    printf("Popped element : %d\n",element);
+            case 2: pop(&stack);
                     break;
-            case 3: element = peek(stack);
-                    printf("Peeked element : %d\n",element);
+            case 3: peek(stack);
                     break;
-            case 4: printf("Stack elements are :\n");
+            case 4: if(isEmpty(stack)){
+                        printf("The stack is currently empty.\n");
+                    }else{
+                        printf("The stack is not empty.\n");
+                    }break;
+            case 5: if(isFull(stack)){
+                        printf("The stack is currently full.\n");
+                    }else{
+                        printf("The stack is not full.\n");
+                    }break;
+            case 6: printf("The elements in the stack are:\n");
                     display(stack);
                     break;
-            case -1:printf("Exiting ...");
+            case -1:printf("Exiting the program...\n");
                     break;
-            default:printf("Invalid Choice!\n");
+            default:printf("Invalid choice! Please choose a valid option.\n");
         }
     }while(choice != -1);
     return 0;
