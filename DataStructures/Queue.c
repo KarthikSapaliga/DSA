0aa7a62c7c530c8df16e669ff9b2dfe81748dca3
Karthik Sapaliga <im.karthiksapaliga@gmail.com>
2024-01-23 19:19:32 +0530

    Queue operations added

--- DataStructures/Queue.c
+++ DataStructures/Queue.c
@@ -15,15 +15,19 @@ void init(Queue *q){
 }
 
 void menu(){
-    printf("===================\n");
+    printf("=============================================\n");
     printf("Queue Operations : \n");
-    printf("-------------------\n");
+    printf("---------------------------------------------\n");
     printf("1. enqueue()\n");
     printf("2. dequeue()\n");
-    printf("3. peek()\n");
-    printf("4. display()\n");
+    printf("3. front()\n");
+    printf("4. rear()\n");
+    printf("5. isEmpty()\n");
+    printf("6. isFull()\n");
+    printf("7. size()\n");
+    printf("8. display()\n");
     printf("\n[0] MENU [-1] EXIT\n");
-    printf("===================\n");
+    printf("=============================================\n");
 }
 
 int isFull(Queue q){
@@ -36,35 +40,47 @@ int isEmpty(Queue q){
 
 void enqueue(Queue *q, int ele){
     if(isFull(*q)){
-        fprintf(stderr,"ERROR : Queue Overflow\n");
-        exit(EXIT_FAILURE);
+        printf("Error : <queue_overflow> cannot insert element.\n");
     }else{
         q->arr[++q->rear] = ele;
+        printf("%d is inserted onto the queue.\n",ele);
     }
 }
 
 int dequeue(Queue *q){
     if(isEmpty(*q)){
-        fprintf(stderr,"ERROR : Queue Underflow\n");
-        exit(EXIT_FAILURE);
+        printf("Error : <queue_underflow> cannot delete element.\n");
     }else{
+        printf("%d is deleted from the queue.\n",q->arr[q->front]);
         return q->arr[q->front++];
     }
 }
 
-int peek(Queue q){
+int front(Queue q){
     if(isEmpty(q)){
-        fprintf(stderr,"ERROR : Queue Underflow\n");
-        exit(EXIT_FAILURE);
+        printf("Error : <empty_queue>\n");
     }else{
+        printf("%d is the front element of the queue.\n",q.arr[q.front]);
         return q.arr[q.front];
     }
 }
 
+int rear(Queue q){
+    if(isEmpty(q)){
+        printf("Error : <empty_queue>\n");
+    }else{
+        printf("%d is the rear element of the queue.\n",q.arr[q.rear]);
+        return q.arr[q.rear];
+    }
+}
+
+int size(Queue q){
+    return q.rear - q.front;
+}
+
 void display(Queue q){
     if(isEmpty(q)){
-        fprintf(stderr,"ERROR : Queue Underflow\n");
-        exit(EXIT_FAILURE);
+        printf("Error : <empty_queue>\n");
     }else{
         for(int i=q.front; i<=q.rear; i++){
             printf("%d ",q.arr[i]);
