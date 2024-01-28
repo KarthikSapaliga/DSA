0c307d2fda194456dedf0c75398690b2cca6508b
Karthik Sapaliga <im.karthiksapaliga@gmail.com>
2024-01-28 10:10:20 +0530

    Update BinarySearch.c

--- Algorithms/Search/BinarySearch.c
+++ Algorithms/Search/BinarySearch.c
@@ -8,11 +8,11 @@ int BinarySearch(int arr[],int n,int target){
         mid = (low + high)/2;
         if(target==arr[mid]){
             return mid;
-        }
-        else if(target<arr[mid])
+        }else if(target<arr[mid]){
             high = mid -1;
-        else 
+        }else{
             low = mid +1;
+        }
     }
     return -1;
 }
